9c8490e6f956014b52d415ff84f169fbd1344f10
pdx <pdx_jie@163.com>
2023-02-12 21:16:00 +0800

    feat:初始化项目

---
+++ src/main/java/com/pdx/radar/AdminRadarApplication.java
@@ -0,0 +1,13 @@
+package com.pdx.radar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminRadarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminRadarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pdx/radar/AdminRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdx.radar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminRadarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
