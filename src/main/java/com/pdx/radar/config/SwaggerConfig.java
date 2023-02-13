c8b7f65760e8faede6862a8176aa24cb92ca365d
pdx <pdx_jie@163.com>
2023-02-14 01:12:07 +0800

    fix:修改Swagger接口文档

--- src/main/java/com/pdx/radar/config/SwaggerConfig.java
+++ src/main/java/com/pdx/radar/config/SwaggerConfig.java
@@ -1,15 +1,19 @@
 package com.pdx.radar.config;
 
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author: 派大星
@@ -27,7 +31,9 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.pdx.radar.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securityContexts(securityContext())
+                .securitySchemes(securitySchemes());
     }
 
     private ApiInfo apiInfo(){
@@ -37,5 +43,36 @@ public class SwaggerConfig {
                 .description("雷达权限管理系统接口文档")
                 .contact(new Contact("派大星","http://localhost:8088/doc.html","pdx_jie@163.com"))
                 .build();
+    }
+
+    private List<ApiKey> securitySchemes(){
+        //设置请求头信息
+        List<ApiKey> result = new ArrayList<>();
+        ApiKey apiKey = new ApiKey("Authorization","Authorization","Header");
+        result.add(apiKey);
+        return result;
+    }
+
+    private List<SecurityContext> securityContext(){
+        //设置需要登录认证的路径
+        List<SecurityContext> contexts = new ArrayList<>();
+        contexts.add(getContextByPath("/hello/.*"));
+        return contexts;
+    }
+
+    private SecurityContext getContextByPath(String pathRegex) {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(pathRegex))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        List<SecurityReference> references = new ArrayList<>();
+        AuthorizationScope authorizationScope = new AuthorizationScope("global","accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        references.add(new SecurityReference("Authorization",authorizationScopes));
+        return references;
     }
 }
