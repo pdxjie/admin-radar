289aabf3b29d227ebdbeb8e2979bc39103ffae51
pdx <pdx_jie@163.com>
2023-02-14 20:35:17 +0800

    feat:添加自定义过滤器 校验访问的url是否拥有所匹配的角色

---
+++ src/main/java/com/pdx/radar/config/CustomFilter.java
@@ -0,0 +1,52 @@
+package com.pdx.radar.config;
+
+import com.pdx.radar.pojo.Menu;
+import com.pdx.radar.pojo.Role;
+import com.pdx.radar.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.util.AntPathMatcher;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @Author: 派大星
+ * @Date: 2023/02/14 2023/2/14
+ * @Description: 根据url分析请求所需要的角色 权限控制
+ */
+public class CustomFilter implements FilterInvocationSecurityMetadataSource {
+
+    @Autowired
+    private MenuService menuService;
+
+    AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    @Override
+    public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
+        String url = ((FilterInvocation) object).getRequestUrl();
+        List<Menu> menus = menuService.getMenusByRole();
+        for (Menu menu : menus) {
+            //判断请求的url与菜单角色是否匹配
+            if (antPathMatcher.match(menu.getUrl(),url)){
+                String[] str = menu.getRoles().stream().map(Role::getName).toArray(String[]::new);
+                return SecurityConfig.createList(str);
+            }
+        }
+        //未匹配的url默认登录即可访问
+        return SecurityConfig.createList("ROLE_LOGIN");
+    }
+
+    @Override
+    public Collection<ConfigAttribute> getAllConfigAttributes() {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+}
