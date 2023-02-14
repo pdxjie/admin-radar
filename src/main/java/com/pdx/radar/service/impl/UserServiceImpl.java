cf929129f5bcf9f1964ce586b433ca29766d9ecb
pdx <pdx_jie@163.com>
2023-02-14 20:05:47 +0800

    fix:用户登录集成验证码

--- src/main/java/com/pdx/radar/service/impl/UserServiceImpl.java
+++ src/main/java/com/pdx/radar/service/impl/UserServiceImpl.java
@@ -5,13 +5,16 @@ import com.pdx.radar.common.DataResult;
 import com.pdx.radar.common.JwtUtils;
 import com.pdx.radar.exception.BusinessException;
 import com.pdx.radar.exception.code.BaseResponseCode;
+import com.pdx.radar.pojo.Menu;
 import com.pdx.radar.pojo.User;
 import com.pdx.radar.mapper.UserMapper;
 import com.pdx.radar.pojo.vo.LoginVo;
 import com.pdx.radar.service.UserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,6 +24,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -36,8 +40,12 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
     @Resource
     private UserDetailsService userDetailsService;
+
     @Resource
     private PasswordEncoder passwordEncoder;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @Resource
     private JwtUtils jwtUtils;
@@ -55,6 +63,19 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     @Override
     public DataResult login(LoginVo loginVo, HttpServletRequest request) {
         UserDetails userDetails = userDetailsService.loadUserByUsername(loginVo.getUsername());
+
+        String captcha = (String) redisTemplate.opsForValue().get("captcha-result");
+        Boolean isExist = redisTemplate.hasKey("captcha-result");
+        //判断验证码是否失效
+        if (!isExist){
+            throw new BusinessException(BaseResponseCode.CODE_IS_NOT_EXISTS);
+        }
+
+        //判断验证码结果是否正确
+        if (!loginVo.getCaptcha().equals(captcha)){
+            throw new BusinessException(BaseResponseCode.CAPTCHA_CODE_IS_ERROR);
+        }
+
         //判断用户是否存在
         if (null == userDetails || !passwordEncoder.matches(loginVo.getPassword(),userDetails.getPassword())){
             throw new BusinessException(BaseResponseCode.USER_IS_NOT_EXIST_OR_ERROR);
