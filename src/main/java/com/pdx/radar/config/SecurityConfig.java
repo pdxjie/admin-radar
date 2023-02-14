6ece1c38cc2eb39d24e388f4ca8918c5ed188040
pdx <pdx_jie@163.com>
2023-02-14 20:57:53 +0800

    fix:增加自定义权限校验过滤器

--- src/main/java/com/pdx/radar/config/SecurityConfig.java
+++ src/main/java/com/pdx/radar/config/SecurityConfig.java
@@ -1,22 +1,27 @@
 package com.pdx.radar.config;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.pdx.radar.exception.BusinessException;
+import com.pdx.radar.exception.code.BaseResponseCode;
+import com.pdx.radar.mapper.RoleMapper;
 import com.pdx.radar.mapper.UserMapper;
 import com.pdx.radar.pojo.User;
-import com.pdx.radar.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.annotation.Resource;
 
 /**
  * @Author: 派大星
@@ -29,11 +34,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserMapper userMapper;
 
+    @Resource
+    private RoleMapper roleMapper;
+
     @Autowired
     private CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Autowired
     private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Resource
+    private CustomFilter customFilter;
+    @Resource
+    private CustomUrlDecisionManager customUrlDecisionManager;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -71,6 +84,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //除了上面的都要登录
                 .anyRequest()
                 .authenticated()
+                //动态权限配置
+                .withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
+                    @Override
+                    public <O extends FilterSecurityInterceptor> O postProcess(O object) {
+                        object.setAccessDecisionManager(customUrlDecisionManager);
+                        object.setSecurityMetadataSource(customFilter);
+                        return object;
+                    }
+                })
                 .and()
                 .headers()
                 .cacheControl();
@@ -92,9 +114,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return username -> {
             User userOne = userMapper.selectOne(new QueryWrapper<User>().eq("username",username));
             if (null != userOne){
+                userOne.setRoles(roleMapper.getRolesById(userOne.getId()));
                 return userOne;
             }
-            return null;
+            throw new BusinessException(BaseResponseCode.USER_IS_NOT_EXIST_OR_ERROR);
         };
     }
 
